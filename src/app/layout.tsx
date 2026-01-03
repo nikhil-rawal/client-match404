decb1d67909cac09cd1788dcb7858de0fec2e119
Nikhil Rawal <59576734+nikhil-rawal@users.noreply.github.com>
2026-01-03 13:24:24 -0500

    updated folder structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import NavBar from "@/components/nav-bar";
-import Footer from "@/components/footer";
+import NavBar from "@/_components/nav-bar";
+import Footer from "@/_components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col`}
       >
         <NavBar />
         {children}
