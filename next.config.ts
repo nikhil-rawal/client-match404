e707f821d5e6561ecd3f2a321e500c7e372e9d2f
nikhil-rawal <rawal.nikhil29@gmail.com>
2026-01-21 23:57:09 -0500

    test4

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: false,
+  transpilePackages: ["daisyui"],
   output: "standalone",
 };
 
